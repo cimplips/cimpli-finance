bec38ea53adb06d62a66dc19f03debdd402695d8
CimpliPS <cimplips@gmail.com>
2026-09-03 13:13:31 +0700

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.example.keuangan_prima"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
